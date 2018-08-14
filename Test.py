c15e786e2e03edf3cfa8fa1c277b1633525d8803
Michael Done <michael.done@sympatico.ca>
2018-08-14 10:51:20 -0400

    Added functionality for water sensor.

--- Test.py
+++ Test.py
@@ -1,2 +1,28 @@
 ## test file for testing things
-# another comment
+import time
+from gpiozero import LED, LineSensor
+from signal import pause
+pump = LED(2)
+water = LineSensor(23)
+water_indicator = LED(24)
+
+def on():
+    pump.on()
+
+def off():
+    pump.off()
+
+def wet():
+    print("wet")
+    water_indicator.on()
+
+def dry():
+    print("dry")
+    water_indicator.off()
+
+water.when_line = dry
+water.when_no_line = wet
+
+off()
+pause()
+
